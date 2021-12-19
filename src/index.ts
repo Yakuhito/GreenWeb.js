c7e8ef5bbf6dd0fdceba4cd1bb912cfb0e383308
yakuhito <y@kuhi.to>
2021-12-19 15:13:39 +0200

    modify index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { LeafletProvider } from "./providers/leaflet";
+
+// greenweb.util
 export { Util as util } from "./util/util";
+
+// greenweb.clvm
 export * as clvm from "clvm";
 
+// greenweb.provider
 export const provider = {
     LeafletProvider,
 };

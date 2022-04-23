f2140357f33473be0ea5212eec0274b68710fd39
yakuhito <y@kuhi.to>
2022-04-23 10:14:07 +0300

    add more tests for Util

--- src/test/util/index.test.ts
+++ src/test/util/index.test.ts
@@ -1,5 +1,10 @@
+/* eslint-disable @typescript-eslint/no-unused-expressions */
 import { expect } from "chai";
 import { Util as util } from "../../util";
+import { AddressUtil } from "../../util/address";
+import { CoinUtil } from "../../util/coin";
+import { NetworkUtil } from "../../util/network";
+import { SerializerUtil } from "../../util/serializer";
 
 describe("Util", function() {
     describe("parseToken", () => {
@@ -106,6 +111,32 @@ describe("Util", function() {
 
         it("Works correctly with NaN", () => {
             expect(util.formatChia(NaN)).to.equal("0.0");
+        });
+    });
+
+    describe("*Util", () => {
+        it("Exposes AddressUtil instance as .address", () => {
+            expect(util.address instanceof AddressUtil).to.be.true;
+        });
+
+        it("Exposes CoinUtil instance as .coin", () => {
+            expect(util.coin instanceof CoinUtil).to.be.true;
+        });
+
+        it("Exposes SerializerUtil instance as .serializer", () => {
+            expect(util.serializer instanceof SerializerUtil).to.be.true;
+        });
+
+        it("Exposes NetworkUtil instance as .network", () => {
+            expect(util.network instanceof NetworkUtil).to.be.true;
+        });
+    });
+
+    describe("Other exposed variables", () => {
+        it("mojoPerXCH", () => {
+            expect(
+                util.mojoPerXCH.eq(1000000000000)
+            ).to.be.true;
         });
     });
 });

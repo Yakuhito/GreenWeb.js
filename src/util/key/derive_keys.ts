67739508fb38adb48f5451ab3245a64b0a725c96
yakuhito <y@kuhi.to>
2022-05-08 18:42:42 +0300

    fix BLS thingy

--- src/util/key/derive_keys.ts
+++ src/util/key/derive_keys.ts
@@ -1,15 +1,17 @@
 // https://github.com/Chia-Network/chia-blockchain/blob/main/chia/wallet/derive_keys.py
 
-import { AugSchemeMPL, G1Element, PrivateKey } from "@chiamine/bls-signatures";
+import { getBLSModule } from "clvm";
 import { Optional } from "../../xch/providers/provider_types";
 
 export class DeriveKeysUtils {
     static MAX_POOL_WALLETS = 100;
 
     private static derivePath(
-        sk: PrivateKey,
+        sk: any,
         path: number[],
-    ): PrivateKey {
+    ): any {
+        const { AugSchemeMPL } = getBLSModule();
+
         for(const index of path) {
             sk = AugSchemeMPL.derive_child_sk(sk, index);
         }
@@ -18,9 +20,11 @@ export class DeriveKeysUtils {
     }
 
     private static derivePathUnhardened(
-        sk: PrivateKey,
+        sk: any,
         path: number[],
-    ): PrivateKey {
+    ): any {
+        const { AugSchemeMPL } = getBLSModule();
+
         for(const index of path) {
             sk = AugSchemeMPL.derive_child_sk_unhardened(sk, index);
         }
@@ -28,46 +32,46 @@ export class DeriveKeysUtils {
         return sk;
     }
 
-    public static masterSkToFarmerSk(master: PrivateKey): PrivateKey {
+    public static masterSkToFarmerSk(master: any): any {
         return this.derivePath(master, [12381, 8444, 0, 0]);
     }
 
-    public static masterSkToPoolSk(master: PrivateKey): PrivateKey {
+    public static masterSkToPoolSk(master: any): any {
         return this.derivePath(master, [12381, 8444, 1, 0]);
     }
     
-    private static masterSkToWalletSkIntermediate(master: PrivateKey): PrivateKey {
+    private static masterSkToWalletSkIntermediate(master: any): any {
         return this.derivePath(master, [12381, 8444, 2]);
     }
 
-    public static masterSkToWalletSk(master: PrivateKey, index: number): PrivateKey {
+    public static masterSkToWalletSk(master: any, index: number): any {
         const intermediate = this.masterSkToWalletSkIntermediate(master);
         return this.derivePath(intermediate, [index]);
     }
 
-    private static masterSkToWalletSkUnhardenedIntermediate(master: PrivateKey): PrivateKey {
+    private static masterSkToWalletSkUnhardenedIntermediate(master: any): any {
         return this.derivePathUnhardened(master, [12381, 8444, 2]);
     }
 
-    public static masterSkToWalletSkUnhardened(master: PrivateKey, index: number): PrivateKey {
+    public static masterSkToWalletSkUnhardened(master: any, index: number): any {
         const intermediate = this.masterSkToWalletSkIntermediate(master);
         return this.derivePathUnhardened(intermediate, [index]);
     }
 
-    public static masterSkToLocalSk(master: PrivateKey): PrivateKey {
+    public static masterSkToLocalSk(master: any): any {
         return this.derivePath(master, [12381, 8444, 3, 0]);
     }
 
-    public static masterSkToBackupSk(master: PrivateKey): PrivateKey {
+    public static masterSkToBackupSk(master: any): any {
         return this.derivePath(master, [12381, 8444, 4, 0]);
     }
 
-    public static masterSkToSingletonOwnerSk(master: PrivateKey, pool_wallet_index: number): PrivateKey {
+    public static masterSkToSingletonOwnerSk(master: any, pool_wallet_index: number): any {
         // This key controls a singleton on the blockchain, allowing for dynamic pooling (changing pools)
         return this.derivePath(master, [12381, 8444, 5, pool_wallet_index]);
     }
 
-    public static masterSkToPoolingAuthenticationSk(master: PrivateKey, pool_wallet_index: number, index: number): PrivateKey {
+    public static masterSkToPoolingAuthenticationSk(master: any, pool_wallet_index: number, index: number): any {
         // This key is used for the farmer to authenticate to the pool when sending partials
         if(index >= 100000) {
             throw new Error("assert index < 10000");
@@ -78,7 +82,7 @@ export class DeriveKeysUtils {
         return this.derivePath(master, [12381, 8444, 6, pool_wallet_index * 10000 + index]);
     }
 
-    public static findOwnerSk(allSks: PrivateKey[], ownerPk: G1Element): Optional<[PrivateKey, number]> {
+    public static findOwnerSk(allSks: any[], ownerPk: any): Optional<[any, number]> {
         for(let poolWalletIndex = 0; poolWalletIndex < this.MAX_POOL_WALLETS; ++poolWalletIndex) {
             for(const sk of allSks) {
                 const tryOwnerSk = this.masterSkToSingletonOwnerSk(sk, poolWalletIndex);
@@ -91,7 +95,7 @@ export class DeriveKeysUtils {
         return null;
     }
 
-    public static findAuthenticationSk(allSks: PrivateKey[], ownerPk: G1Element): Optional<PrivateKey> {
+    public static findAuthenticationSk(allSks: any[], ownerPk: any): Optional<any> {
         // NOTE: might need to increase this if using a large number of wallets, or have switched authentication keys
         // many times.
         for(let poolWalletIndex = 0; poolWalletIndex < this.MAX_POOL_WALLETS; ++poolWalletIndex) {

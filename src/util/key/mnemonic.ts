b6890d12217cd48f4fc3b31ed67870c9256eed5f
yakuhito <y@kuhi.to>
2022-05-11 10:44:26 +0300

    MnemonicUtils: mnemonicToSeed, privateKeyFromMnemonic

--- src/util/key/mnemonic.ts
+++ src/util/key/mnemonic.ts
@@ -1,6 +1,7 @@
 import { bytes } from "../../xch/providers/provider_types";
 import CryptoJS from "crypto-js";
 import { BIP39_WORD_LIST } from "./bip39_word_list";
+import { getBLSModule } from "clvm";
 
 export class MnemonicUtils {
     private static stdHash(data: bytes): bytes {
@@ -13,6 +14,7 @@ export class MnemonicUtils {
         );
     }
 
+    // https://github.com/Chia-Network/chia-blockchain/blob/main/chia/util/keychain.py#L147
     public static bytesToMnemonic(mnemonicBytes: bytes): string {
         if(![16, 20, 24, 28, 32].includes(mnemonicBytes.length / 2)) {
             throw new Error(`Data length should be one of the following: [16, 20, 24, 28, 32], but it is ${mnemonicBytes.length / 2}.`);
@@ -38,6 +40,7 @@ export class MnemonicUtils {
         return mnemonics.join(" ");
     }
 
+    // https://github.com/Chia-Network/chia-blockchain/blob/main/chia/util/keychain.py#L172
     public static bytesFromMnemonic(mnemonicStr: string): bytes {
         const mnemonic: string[] = mnemonicStr.split(" ");
         if(![12, 15, 18, 21, 24].includes(mnemonic.length)) {
@@ -73,5 +76,33 @@ export class MnemonicUtils {
         }
 
         return entropyBytes;
+    }
+
+    // https://github.com/Chia-Network/chia-blockchain/blob/main/chia/util/keychain.py#L203
+    public static mnemonicToSeed(mnemonic: string, passphrase: string) {
+        // Uses BIP39 standard to derive a seed from entropy bytes.
+        const saltStr = "mnemonic" + passphrase;
+        const salt = saltStr.normalize("NFKD");
+        const mnemonicNormalized = mnemonic.normalize("NFKD");
+
+        const res =  CryptoJS.enc.Hex.stringify(
+            CryptoJS.PBKDF2(salt, mnemonicNormalized, {
+                iterations: 2048,
+                hasher: CryptoJS.algo.SHA512
+            })
+        );
+        
+        console.log({assert: res.length, equalTo: 64}) //c
+        return res;
+    }
+
+    // https://github.com/Chia-Network/chia-blockchain/blob/a4ec7e7cb8d8ecfa2a50c038e5dd94ef3e620c6c/chia/cmds/keys_funcs.py#L644
+    public static privateKeyFromMnemonic(mnemonic: string): any {
+        const { AugSchemeMPL } = getBLSModule();
+
+        const seed = this.mnemonicToSeed(mnemonic, "");
+        return AugSchemeMPL.key_gen(
+            Buffer.from(seed, "hex")
+        );
     }
 }
